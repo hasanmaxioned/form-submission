45e54a880b58db565abaeac314a7175e0a5240be
hasanmaxioned <hasanm@axioned.com>
2022-12-09 11:17:15 +0530

    add js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,74 @@
 let regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
-let addressRegex = /^[a-zA-Z0-9\s\,\''\-]*$/;
+let addressRegex = /^[a-zA-Z02-9]$/;
 
 let firstName = document.querySelector(".first-name");
 let lastName = document.querySelector(".last-name");
 let male = document.querySelector(".male-gender");
 let female = document.querySelector(".female-gender");
 let address = document.querySelector(".address-field");
-let check =document.querySelector(".check-box-field");
+let check = document.querySelector(".check-box-field");
 let isValid;
 document.querySelector(".form").addEventListener("submit", function (e) {
   e.preventDefault();
-  validate(firstName,regName, 4, 15);
-  validate(lastName,regName, 4, 15);
+  validate(firstName, regName, 4, 15);
+  validate(lastName, regName, 4, 15);
   radioValidate();
-  validate(address,addressRegex, 4, 60);
+  validate(address, addressRegex, 4, 60);
   checkValidate();
-  if(isValid == true) {
+  if (isValid == true) {
     alert("your form has been submitted");
+    form.reset();
   }
-
-})
-
+});
 
 function validate(input, regex, min, max) {
- const inputGroup = input.parentElement;
- errorMessage = inputGroup.querySelector(".error");
-//  console.log(inputGroup);
-isValid = true;
- if(input.value == "" ){
-   errorMessage.classList.add("active");
-   inputGroup.classList.add("active");
-   errorMessage.innerText = "*Field is required";
-   return isValid = false;
-
- }else if(input.value.length < min || input.value.length > max) {
-  errorMessage.classList.add("active");
-   inputGroup.classList.add("active");
-   errorMessage.innerText = "*It must be between " + min + " & " + max;
-   return isValid = false;
- }else if(!regex.test(input.value)) {
-  errorMessage.classList.add("active");
-   inputGroup.classList.add("active");
-   errorMessage.innerText = "*please Enter your valid " + input.name;
-   return isValid = false;
- }else {
-  // console.log("remove");
+  const inputGroup = input.parentElement;
+  errorMessage = inputGroup.querySelector(".error");
+  //  console.log(inputGroup);
+  isValid = true;
+  if (input.value == "") {
+    errorMessage.classList.add("active");
+    inputGroup.classList.add("active");
+    errorMessage.innerText = "*Field is required";
+    return (isValid = false);
+  } else if (input.value.length < min || input.value.length > max) {
+    errorMessage.classList.add("active");
+    inputGroup.classList.add("active");
+    errorMessage.innerText = "*It must be between " + min + " & " + max;
+    return (isValid = false);
+  } else if (regex.test(input.value)) {
+    errorMessage.classList.add("active");
+    inputGroup.classList.add("active");
+    errorMessage.innerText = "*please Enter your valid " + input.name;
+    return (isValid = false);
+  } else {
     errorMessage.classList.remove("active");
     inputGroup.classList.remove("active");
- }
- return isValid;
-} 
+    // console.log();
+  }
+  return isValid;
+}
 function radioValidate() {
-const radioError = document.querySelector(".radio-error");
- if( (male.checked != true) && (female.checked != true)) {
-  radioError.classList.add("active");
-  radioError.innerText = "*please select your gender ";
-  return isValid = false;
- }else {
-  radioError.classList.remove("active");
-  return isValid = true;
- }
+  const radioError = document.querySelector(".radio-error");
+  if (male.checked != true && female.checked != true) {
+    radioError.classList.add("active");
+    radioError.innerText = "*please select your gender ";
+    return (isValid = false);
+  } else {
+    radioError.classList.remove("active");
+    return (isValid = true);
+  }
 }
 
 function checkValidate() {
   const checkError = document.querySelector(".check-error");
-   if(check.checked != true) {
+  if (check.checked != true) {
     checkError.classList.add("active");
     checkError.innerText = "*please select our terms and condition ";
-    return isValid = false;
-   }else {
+    return (isValid = false);
+  } else {
     checkError.classList.remove("active");
-    return isValid = true;
-   }
+    return (isValid = true);
   }
+}
+
