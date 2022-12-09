9b858dddab18904509a522a27690c5b74eeba7f7
hasanmaxioned <hasanm@axioned.com>
2022-12-09 16:29:39 +0530

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,25 +7,46 @@ let male = document.querySelector(".male-gender");
 let female = document.querySelector(".female-gender");
 let address = document.querySelector(".address-field");
 let check = document.querySelector(".check-box-field");
+let arr = [];
+let inputVal = document.querySelectorAll(".form input");
+let dataStore = document.querySelector(".data-store");
 let isValid;
 document.querySelector(".form").addEventListener("submit", function (e) {
   e.preventDefault();
-  validate(firstName, regName, 4, 15);
-  validate(lastName, regName, 4, 15);
+  validate(firstName, regName, 3, 15);
+  validate(lastName, regName, 3, 15);
   radioValidate();
   validate(address, addressRegex, 4, 60);
   checkValidate();
   if (isValid == true) {
     alert("your form has been submitted");
+    let user = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      radio: (male.checked ? male.value : female.value),
+      addr: address.value
+    };
+    arr.push(user);
+    formData(arr);
     form.reset();
+    console.log(arr);
   }
+});
+firstName.addEventListener("blur", function (e) {
+  validate(firstName, regName, 3, 15);
+});
+lastName.addEventListener("blur", function (e) {
+  validate(lastName, regName, 3, 15);
+});
+address.addEventListener("blur", function (e) {
+  validate(address, addressRegex, 4, 60);
 });
 
 function validate(input, regex, min, max) {
   const inputGroup = input.parentElement;
   errorMessage = inputGroup.querySelector(".error");
-  //  console.log(inputGroup);
   isValid = true;
+
   if (input.value == "") {
     errorMessage.classList.add("active");
     inputGroup.classList.add("active");
@@ -44,7 +65,6 @@ function validate(input, regex, min, max) {
   } else {
     errorMessage.classList.remove("active");
     inputGroup.classList.remove("active");
-    // console.log();
   }
   return isValid;
 }
@@ -72,3 +92,41 @@ function checkValidate() {
   }
 }
 
+function formData(arr) {
+  let empty = document.querySelectorAll(".data");
+  if (empty) {
+    empty.forEach(function (emp) {
+      emp.parentElement.removeChild(emp);
+    });
+  }
+  arr.forEach(function (element) {
+    let dataStore = document.querySelector(".data-store");
+    let dataList = document.createElement("li");
+    dataList.className = "data";
+    dataList.innerHTML = `<ul class="detail-list">
+    <li class="details">${element.firstName}</li>
+    <li class="details">${element.lastName}</li>
+    <li class="details">${element.radio}</li>
+    <li class="details">${element.addr}</li>
+    <li class="details"><button class="edit-btn">edit</button></li>
+    <li class="details"><button class="del-btn">delete</button></li>
+  </ul>`;
+    dataStore.appendChild(dataList);
+    const delBtn = dataList.querySelector(".del-btn");
+    delBtn.addEventListener("click", function () {
+      dataList.remove();
+    });
+
+    const editBtn = dataList.querySelector(".edit-btn");
+    editBtn.addEventListener("click", function () {
+      firstName.value = element.firstName;
+      lastName.value = element.lastName;
+      address.value = element.address;
+      if (male.checked === element.radio) {
+        male.checked = true;
+      } else {
+        female.checked = true;
+      }
+    });
+  });
+}
